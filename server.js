739c3b852504d977ceeffc6ba049bc2cfed0fe97
ToGermanScore Ltd <yousef@togermanscore.com>
2022-07-01 14:49:52 +0200

    init

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const app = express();
 app.set('query parser', 'simple');
 app.use(express.json());
 
-app.get('/score', (req, res) => { handleScore(req, res) });
+app.get('/', (req, res) => { handleScore(req, res) });
 
 app.listen(port, () => console.log('running on port', port));
